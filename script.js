a61a9516a81f319d141d9b38bbf24f4a69d69036
Abhishek <abhisheksangule6@gmail.com>
2024-11-01 14:16:53 +0530

    add all playlist songs

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ console.log("get started with JavaScript")
 const mostContainer = document.querySelector('.most');
 const songsListContainer = document.querySelector('.songs_list');
 const listItems = mostContainer.querySelectorAll('li');
+// console.log(listItems[0])
 const homeButton = document.querySelector('#home_btn');
 
 // Function to show songs list and hide most container
@@ -64,7 +65,7 @@ listItems1.forEach((item, index) => {
 
 
 // Array holding song data
-const songsData = [
+const LikedSongsData = [
     {
       title: "Kalank Title Track",
       playlist: "Liked Songs",
@@ -121,6 +122,200 @@ const songsData = [
     }
     // Add more songs as needed
   ];
+const sandeepMaheshwari = [
+    {
+      title: "BE FEARLESS - Motivational Video",
+      playlist: "sandeepMaheshwari",
+      singer: "sandeep Maheshwari",
+      filePath: "songs/sandeepMaheshwari/BE FEARLESS - Motivational Video By Sandeep Maheshwari.mp3"
+    },
+    {
+      title: "MORNING MOTIVATIONAL VIDEO",
+      playlist: "sandeepMaheshwari",
+      singer: "sandeep Maheshwari",
+      filePath: "songs/sandeepMaheshwari/MORNING MOTIVATIONAL VIDEO - Sandeep Maheshwari  DAILY MORNING AFFIRMATIONS Hindi.mp3"
+    },
+    {
+      title: "POWERFUL MOTIVATIONAL VIDEO By Sandeep Maheshwari",
+      playlist: "sandeepMaheshwari",
+      singer: "sandeep Maheshwari",
+      filePath: "songs/sandeepMaheshwari/POWERFUL MOTIVATIONAL VIDEO By Sandeep Maheshwari  Best Motivational Quotes.mp3"
+    }
+    
+    // Add more songs as needed
+  ];
+const Arijit_singh = [
+    {
+      title: "Kalank Title Track",
+      playlist: "Liked Songs",
+      singer: "Arijit Singh",
+      filePath: "songs/liked_Songs/Kalank Title Track - Lyrical  Alia Bhatt , Varun Dhawan  Arijit Singh  Pritam Amitabh.mp3"
+    },
+    {
+      title: "ANIMAL_ SATRANGA(Song)",
+      playlist: "Liked Songs",
+      singer: "Armaan Malik",
+      filePath: "songs\Arijit_Singh\ANIMAL_ SATRANGA(Song) Ranbir Kapoor,Rashmika_Sandeep V_Arijit,Shreyas P,Siddharth-Garima _Bhushan K [ ezmp3.cc ].mp3"
+    },
+    {
+      title: "Chal Wahan Jaate Hain",
+      playlist: "Liked Songs",
+      singer: "Arijit Singh",
+      filePath: "songs/liked_Songs/Chal Wahan Jaate Hain Full Song with LYRICS - Arijit Singh  Tiger Shroff, Kriti Sanon  T-Series.mp3"
+    },
+    {
+      title: "Dil Meri Na Sune Lyrical",
+      playlist: "Liked Songs",
+      singer: "Arijit Singh",
+      filePath: "songs/liked_Songs/Dil Meri Na Sune Lyrical - Genius  Utkarsh Sharma, Ishita  Atif Aslam  Himesh Reshammiya.mp3"
+    },
+    {
+      title: "Mohe Rang Do Laal",
+      playlist: "Liked Songs",
+      singer: "Arijit Singh",
+      filePath: "songs/liked_Songs/Mohe Rang Do Laal (Official Video Song)  Bajirao Mastani  Ranveer Singh & Deepika Padukone.mp3"
+    },
+    {
+      title: "Tere Pyaar Mein",
+      playlist: "Liked Songs",
+      singer: "Arijit Singh",
+      filePath: "songs/liked_Songs/Tere Pyaar Mein (Song) Tu Jhoothi Main Makkaar Ranbir, Shraddha Pritam Arijit, Nikhita  Amitabh.mp3"
+    },
+    {
+      title: "Apna Bana Le",
+      playlist: "Liked Songs",
+      singer: "Arijit Singh",
+      filePath: "songs\Arijit_Singh\Apna Bana Le - Bhediya _ Varun Dhawan, Kriti Sanon_ Sachin-Jigar, Arijit Singh, Amitabh Bhattacharya [ ezmp3.cc ].mp3"
+    },
+    {
+      title: "Sajni (Song)_ Arijit Singh ",
+      playlist: "Liked Songs",
+      singer: "Arijit Singh",
+      filePath: "songs\Arijit_Singh\Sajni (Song)_ Arijit Singh, Ram Sampath  Laapataa Ladies   Aamir Khan Productions.mp3"
+    },
+    {
+      title: "Song Title 2",
+      playlist: "Liked Songs",
+      singer: "Arijit Singh",
+      filePath: "path/to/song2.mp3"
+    }
+    // Add more songs as needed
+  ];
+const Arijit_topTrack = [
+    {
+      title: "Kalank Title Track",
+      playlist: "Liked Songs",
+      singer: "Arijit Singh",
+      filePath: "songs/liked_Songs/Kalank Title Track - Lyrical  Alia Bhatt , Varun Dhawan  Arijit Singh  Pritam Amitabh.mp3"
+    },
+    {
+      title: "BESABRIYAAN",
+      playlist: "Liked Songs",
+      singer: "Armaan Malik",
+      filePath: "songs/liked_Songs/BESABRIYAAN Full Video Song  M. S. DHONI - THE UNTOLD STORY  Sushant Singh Rajput.mp3"
+    },
+    {
+      title: "Chal Wahan Jaate Hain",
+      playlist: "Liked Songs",
+      singer: "Arijit Singh",
+      filePath: "songs/liked_Songs/Chal Wahan Jaate Hain Full Song with LYRICS - Arijit Singh  Tiger Shroff, Kriti Sanon  T-Series.mp3"
+    },
+    {
+      title: "Dil Meri Na Sune Lyrical",
+      playlist: "Liked Songs",
+      singer: "Arijit Singh",
+      filePath: "songs/liked_Songs/Dil Meri Na Sune Lyrical - Genius  Utkarsh Sharma, Ishita  Atif Aslam  Himesh Reshammiya.mp3"
+    },
+    {
+      title: "Mohe Rang Do Laal",
+      playlist: "Liked Songs",
+      singer: "Arijit Singh",
+      filePath: "songs/liked_Songs/Mohe Rang Do Laal (Official Video Song)  Bajirao Mastani  Ranveer Singh & Deepika Padukone.mp3"
+    },
+    {
+      title: "Tere Pyaar Mein",
+      playlist: "Liked Songs",
+      singer: "Arijit Singh",
+      filePath: "songs/liked_Songs/Tere Pyaar Mein (Song) Tu Jhoothi Main Makkaar Ranbir, Shraddha Pritam Arijit, Nikhita  Amitabh.mp3"
+    },
+    {
+      title: "The Humma Song",
+      playlist: "Liked Songs",
+      singer: "jubin nautiyal",
+      filePath: "songs/liked_Songs/The Humma Song  OK Jaanu  Shraddha Kapoor  Aditya Roy Kapur  @ARRahman, Badshah, Tanishk.mp3"
+    },
+    {
+      title: "WAJAH TUM HO ",
+      playlist: "Liked Songs",
+      singer: "Armaan Malik",
+      filePath: "songs/liked_Songs/WAJAH TUM HO Full Video Song  HATE STORY 3 Songs  Zareen Khan, Karan Singh Grover  T-Series.mp3"
+    },
+    {
+      title: "Song Title 2",
+      playlist: "Liked Songs",
+      singer: "Arijit Singh",
+      filePath: "path/to/song2.mp3"
+    }
+    // Add more songs as needed
+  ];
+const Sonu_Nigam = [
+    {
+      title: "Kalank Title Track",
+      playlist: "Liked Songs",
+      singer: "Arijit Singh",
+      filePath: "songs\sonu_nigam\Ajay-Atul - Abhi Mujh Mein Kahin Best Lyric_Agneepath_Priyanka Chopra,Hrithik_Sonu Nigam [ ezmp3.cc ].mp3"
+    },
+    {
+      title: "BESABRIYAAN",
+      playlist: "Liked Songs",
+      singer: "Armaan Malik",
+      filePath: "songs\sonu_nigam\Kal Ho Naa Ho Full Video - Title Track_Shah Rukh Khan,Saif Ali,Preity_Sonu Nigam_Karan J [ ezmp3.cc ].mp3"
+    },
+    {
+      title: "Chal Wahan Jaate Hain",
+      playlist: "Liked Songs",
+      singer: "Arijit Singh",
+      filePath: "songs\sonu_nigam\LYRICAL_ Main Agar Kahoon _ Om Shanti Om _ Shahrukh Khan,Deepika Padukone _ Sonu Nigam,Shreya Ghosal [ ezmp3.cc ].mp3"
+    },
+    {
+      title: "Dil Meri Na Sune Lyrical",
+      playlist: "Liked Songs",
+      singer: "Arijit Singh",
+      filePath: "songs\sonu_nigam\Tumse Milke Dilka Jo Haal [Full Song] _ Main Hoon Na _ Shahrukh Khan [ ezmp3.cc ].mp3"
+    },
+    {
+      title: "Mohe Rang Do Laal",
+      playlist: "Liked Songs",
+      singer: "Arijit Singh",
+      filePath: "songs\sonu_nigam\संदेशे आते है (4K) - Sandese Aate Hai Full 4K Video Song _ Border _ बॉर्डर - सनी देओल [ ezmp3.cc ].mp3"
+    },
+    {
+      title: "Tere Pyaar Mein",
+      playlist: "Liked Songs",
+      singer: "Arijit Singh",
+      filePath: "songs/liked_Songs/Tere Pyaar Mein (Song) Tu Jhoothi Main Makkaar Ranbir, Shraddha Pritam Arijit, Nikhita  Amitabh.mp3"
+    },
+    {
+      title: "The Humma Song",
+      playlist: "Liked Songs",
+      singer: "jubin nautiyal",
+      filePath: "songs/liked_Songs/The Humma Song  OK Jaanu  Shraddha Kapoor  Aditya Roy Kapur  @ARRahman, Badshah, Tanishk.mp3"
+    },
+    {
+      title: "WAJAH TUM HO ",
+      playlist: "Liked Songs",
+      singer: "Armaan Malik",
+      filePath: "songs/liked_Songs/WAJAH TUM HO Full Video Song  HATE STORY 3 Songs  Zareen Khan, Karan Singh Grover  T-Series.mp3"
+    },
+    {
+      title: "Song Title 2",
+      playlist: "Liked Songs",
+      singer: "Arijit Singh",
+      filePath: "path/to/song2.mp3"
+    }
+    // Add more songs as needed
+  ];
+
   
   // Create an audio player to reuse for playing songs
  
@@ -139,11 +334,11 @@ const songsData = [
   const pauseBtn = musicPlayer.querySelector('#pause');
   
   // Function to populate the songs list
-  function populateSongsList() {
+  function populateSongsList(data_playlist) {
     const songsListUl = document.querySelector('.songs_list ul');
     songsListUl.innerHTML = '';
   
-    songsData.forEach((song, index) => {
+    data_playlist.forEach((song, index) => {
       const li = document.createElement('li');
       li.classList.add('song_card');
   
@@ -257,5 +452,29 @@ const songsData = [
   });
   
   // Initial call to populate the list
-  populateSongsList();
+
+  listItems[0].addEventListener("click", ()=>{
+
+    populateSongsList(LikedSongsData);
+  })
+  listItems[1].addEventListener("click", ()=>{
+
+    populateSongsList(sandeepMaheshwari);
+  })
+  listItems[2].addEventListener("click", ()=>{
+
+    populateSongsList(Arijit_singh);
+  })
+  listItems[3].addEventListener("click", ()=>{
+
+    populateSongsList(Arijit_topTrack);
+  })
+  listItems[4].addEventListener("click", ()=>{
+
+    populateSongsList(Sonu_Nigam);
+  })
+  listItems[5].addEventListener("click", ()=>{
+
+    populateSongsList(LikedSongsData);
+  })
   
